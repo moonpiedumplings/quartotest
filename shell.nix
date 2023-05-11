162be66e822f9c010d55738cbfd2fc454464bd11
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-05-11 11:29:56 -0700

    cleaned a little formatting of my shell.nix.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} } :
     pkgs.mkShell {
-        packages = [ pkgs.quarto pkgs.jupyter pkgs.pandoc pkgs.deno pkgs.mkpasswd ];
+        packages = with pkgs; [ quarto jupyter pandoc deno mkpasswd ];
     }
