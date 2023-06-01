72603459d2f40d88657d97be187cbcae3c7fc574
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-06-01 10:02:16 -0700

    more changes

--- funny2.nix
+++ funny2.nix
@@ -1,9 +1,9 @@
 let
 	pkgs = import (fetchTarball {
-		url = "https://github.com/seam345/nixpkgs/archive/89e6e477c8357a087e863db562d2fa8d9fe5ba29.tar.gz";
-		sha256 = "1jjkfzjls2b363lxad8xigqigfkzwyvdrr4d3m84vbs9s4xa7hp2";
+		url = "https://github.com/nixos/nixpkgs/archive/138d6b446388e85f3f7d8c0d6661a46519aa3530.tar.gz";
+		sha256 = "17adlrdnlk25ihwixjm6x442f73ky89827nw3wzvlvc9h2chs3yq";
 	}) {};
 in
 	pkgs.mkShell {
-		packages = with pkgs; [ haskellPackages.pandoc-cli quarto jupyter deno ];
+		packages = with pkgs; [ haskellPackages.pandoc_3_1_2 ];
 	}
--- funny3.nix
+++ funny3.nix
@@ -1,10 +1,17 @@
-let
-	haskellPackages.pandoc-cli = {pkgs ? import (fetchTarball {
+{ pkgs ? import (fetchTarball {
 		url = "https://github.com/seam345/nixpkgs/archive/89e6e477c8357a087e863db562d2fa8d9fe5ba29.tar.gz";
 		sha256 = "1jjkfzjls2b363lxad8xigqigfkzwyvdrr4d3m84vbs9s4xa7hp2";
-	}) {} }: pkgs.haskellPackages.pandoc-cli;
-	pkgs = import <nixpkgs> {};
+	}) {} } :
+let
+	pandoc = pkgs.haskellPackages.pandoc-cli;
+	quarto = (pkgs.quarto.overrideAttrs (oldAttrs: rec {
+        version = "1.3.361";
+        src = pkgs.fetchurl {
+            url = "https://github.com/quarto-dev/quarto-cli/releases/download/v${version}/quarto-${version}-linux-amd64.tar.gz";
+            sha256 = "sha256-vvnrIUhjsBXkJJ6VFsotRxkuccYOGQstIlSNWIY5nuE=";
+        };
+    })).override {inherit pandoc;};
 in
 	pkgs.mkShell {
-		packages = with pkgs; [ haskellPackages.pandoc-cli quarto jupyter deno ];
+		packages = [ quarto ];
 	}
