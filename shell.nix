adb2d60f7ca9ee708657acf39f7157abe0cc7e87
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-06-07 18:35:48 -0700

    finished quarto via nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
-{
-    pkgs ? import <nixpkgs> {},
-} :
-
-let quarto = pkgs.callPackage ./env/quarto.nix {}; in
-pkgs.mkShell {
-    packages = with pkgs; [ python310Full quarto jupyter ];
-}
+let
+    pkgs = import <nixpkgs> {};
+    quarto = pkgs.callPackage ./env/quarto.nix {};
+in
+    pkgs.mkShell {
+        packages = with pkgs; [ python310Full quarto jupyter ];
+    }
